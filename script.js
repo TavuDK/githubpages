cccebc2ad2bc490ab7c3057878bd60865d2f9da1
Chris <kaymaxmail@proton.me>
2024-07-02 00:52:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    var savedLang = localStorage.getItem('selectedLang');
+    var savedFlag = localStorage.getItem('selectedFlag');
+    if (savedLang && savedFlag) {
+        $('#currentFlag').attr('src', savedFlag);
+    }
+
+    $('.dropdown-item').on('click', function() {
+        var lang = $(this).data('lang');
+        var flagSrc = $(this).find('img').attr('src');
+        
+        // opdater flagikonet
+        $('#currentFlag').attr('src', flagSrc);
+        
+        // gem valgt sprog og flag i lokal lagring
+        localStorage.setItem('selectedLang', lang);
+        localStorage.setItem('selectedFlag', flagSrc);
+        
+        if (lang === 'da') {
+            alert('Sproget er skiftet til Dansk.');
+        } else if (lang === 'en') {
+            alert('Language changed to English.');
+        }
+    });
+});
