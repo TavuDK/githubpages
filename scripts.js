cce67a582351f1e068f9390ecd54f6ed27718e1d
Chris <kaymaxmail@proton.me>
2024-07-22 01:45:56 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -24,44 +24,5 @@ $(document).ready(function() {
     });
 });
 
-function showLoader() {
-    document.getElementById('loader').style.display = 'block';
-}
 
-function hideLoader() {
-    document.getElementById('loader').style.display = 'none';
-}
-
-function performTask() {
-    showLoader();
-
-    setTimeout(() => {
-
-        hideLoader();
-    }, 2000);
-}
-
-const originalConsoleError = console.error;
-console.error = function(...args) {
-
-    originalConsoleError.apply(console, args);
-
-    showLoader();
-
-    setTimeout(() => hideLoader(), 2000);
-};
-
-async function asyncOperation() {
-    showLoader();
-    try {
-
-        await new Promise(resolve => setTimeout(resolve, 2000));
-    } catch (error) {
-        console.error(error); 
-    } finally {
-        hideLoader();
-    }
-}
-
-performTask();
 
